bc227bf48ebda51320b92fdc397ab574e8b3102a
Rachakonda.Vamshi <146413004+rvamshi01@users.noreply.github.com>
2024-02-21 11:07:32 +0530

    Update productmodel.js

--- models/productmodel.js
+++ models/productmodel.js
@@ -1,4 +1,4 @@
-const mongoose  = required("mongoose")
+const mongoose  = require("mongoose")
 const productmodel=mongoose.Schema({
     name:{type:String,required:true},
     description:{type:String,required:true},
