d794d3b9be9cc5d0c97a353ebda39574a8ae6ccf
Rachakonda.Vamshi <146413004+rvamshi01@users.noreply.github.com>
2024-03-03 10:17:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ app.use(cors())
 app.use("/auth",require("./routes/Authentication"));
 app.use("/products",require("./routes/Products"));
 app.listen(process.env.PORT,()=>{
-    console.log("listening on port: ",process.env.PORT)
+    console.log("listening on port: ",process.env.PORT);
 })
  
