d899812da1476ed74f1cc1b07b78a2f51fa90235
Rachakonda.Vamshi <146413004+rvamshi01@users.noreply.github.com>
2024-03-07 09:31:18 +0530

    Update dbConnection.js

--- dbConnection.js
+++ dbConnection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connect = async()=>{
     try{
-        await mongoose.connect(process.env.CONNECTION_STRING)
+        await mongoose.connect(process.env.CONNECTION_STRING);
         console.log("connected Database")
 
     }
